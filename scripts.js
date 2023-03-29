068941478c2d8b9dbf01d95ae9e2bb0fdc7598e9
DerangedDoctor <zackyzeke@gmail.com>
2023-03-29 15:25:24 -0500

    hide/show
    properly hide and show elements based on selected

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
-function display() {
-  document.getElementByClassName("about").style.display = "block";
-}
+function show(x) {
+  var elements = ["welcome", "about", "certifications", "projects", "contact"];
+  if (document.getElementById(x).style.display === "block") {
+  document.getElementById("welcome").style.display = "block";
+  document.getElementById(x).style.display = "none";
+  } else { 
+    for (var i = 0; i < elements.length; i++) {
+      document.getElementById(elements[i]).style.display = "none";
+    }
+  // set display value of the element with ID x to "block"
+  document.getElementById(x).style.display = "block";
+  } }
