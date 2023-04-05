3fe039ad75f13fba0337844c93a273bfee7e56e4
DerangedDoctor <zackyzeke@gmail.com>
2023-04-05 12:49:32 -0500

    hide/show changes by class
    added hidden and visible classes to assign to targeted element, removed script that changed display from none to  block, also added fade in and out effect when changing

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
+
+// cycles between showing and hiding the textbox elements
+//if the element is already visible, it will hide it and show the welcome element
+//if the element is hidden, it will show it and hide the welcome element
 function show(x) {
   var elements = ["welcome", "about", "certifications", "projects", "contact"];
-  if (document.getElementById(x).style.display === "block") {
-  document.getElementById("welcome").style.display = "block";
-  document.getElementById(x).style.display = "none";
+  if (document.getElementById(x).classList.contains("visible")) {
+  document.getElementById("welcome").classList.add("visible");
+  document.getElementById("welcome").classList.remove("hidden");
+  document.getElementById(x).classList.add("hidden");
+  document.getElementById(x).classList.remove("visible");
   } else { 
     for (var i = 0; i < elements.length; i++) {
-      document.getElementById(elements[i]).style.display = "none";
+      document.getElementById(elements[i]).classList.add("hidden");
+      document.getElementById(elements[i]).classList.remove("visible");
     }
-  // set display value of the element with ID x to "block"
-  document.getElementById(x).style.display = "block";
-  } }
+  document.getElementById(x).classList.add("visible");
+  document.getElementById(x).classList.remove("hidden");
+  } };
