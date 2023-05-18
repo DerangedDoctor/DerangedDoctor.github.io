9bc7833304c9ea644ccfe5dd7d203a5db99739c6
DerangedDoctor <38366172+DerangedDoctor@users.noreply.github.com>
2023-05-18 10:36:50 -0500

    simplify js
    simplified js code and added comments

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -1,40 +1,43 @@
-// cycles between showing and hiding the textbox elements
-//if the element is already visible, it will hide it and show the welcome element
-//if the element is hidden, it will show it and hide the welcome element
-function show(x) {
-let idOfX = document.getElementById(x);
-let idOfWelcome = document.getElementById("welcome");
-let elements = ["welcome", "about", "certifications", "projects", "contact"];
-  if (idOfX.classList.contains("visible")) {
-    idOfWelcome.classList.add("visible");
-    idOfWelcome.classList.remove("hidden");
-    idOfX.classList.add("hidden");
-    idOfX.classList.remove("visible");
-  } else { 
-    for (let i = 0; i < elements.length; i++) {
-      document.getElementById(elements[i]).classList.add("hidden");
-      document.getElementById(elements[i]).classList.remove("visible");
-    }
-    idOfX.classList.add("visible");
-    idOfX.classList.remove("hidden");
-  } };
 
-  function setActiveId(x) {
-    //adds 1 to the end of the id to match the id of the corresponding button
-    x = x + "1";    
-    let idOfX = document.getElementById(x);
-    //if the element is already active, it will remove the active class
-    if (idOfX.classList.contains("active")) {
-      idOfX.classList.remove("active");
-    } else {
-      let elements = ["about1", "certifications1", "projects1", "contact1"];
-      for (let j = 0; j < elements.length; j++) {
-        document.getElementById(elements[j]).classList.remove("active");
-      }
-      idOfX.classList.add("active");
-    }
-  };
-  function showAndSetActive(x) {
-    show(x);
-    setActiveId(x);
-  };   
+// This function shows the element with the given id and hides all other elements
+function show(x) {
+  const idOfX = document.getElementById(x);
+  const idOfWelcome = document.getElementById("welcome");
+  const elements = ["welcome", "about", "certifications", "projects", "contact"];
+  
+  // If the element is already visible, hide it and show the welcome element
+  if (idOfX.classList.contains("visible")) {
+    idOfWelcome.classList.replace("hidden", "visible");
+    idOfX.classList.replace("visible", "hidden");
+  } else {
+    // Otherwise, hide all other elements and show the selected element
+    elements.forEach((element) => {
+      document.getElementById(element).classList.replace("visible", "hidden");
+    });
+    idOfX.classList.replace("hidden", "visible");
+  }
+}
+
+// This function sets the active class for the element with the given id and removes it from all other elements
+function setActiveId(x) {
+  x = x + "1";
+  const idOfX = document.getElementById(x);
+  
+  // If the element is already active, remove the active class
+  if (idOfX.classList.contains("active")) {
+    idOfX.classList.remove("active");
+  } else {
+    // Otherwise, remove the active class from all other elements and add it to the selected element
+    const elements = ["about1", "certifications1", "projects1", "contact1"];
+    elements.forEach((element) => {
+      document.getElementById(element).classList.remove("active");
+    });
+    idOfX.classList.add("active");
+  }
+}
+
+// This function shows the element with the given id, hides all other elements, and sets the active class for the selected element
+function showAndSetActive(x) {
+  show(x);
+  setActiveId(x);
+}
